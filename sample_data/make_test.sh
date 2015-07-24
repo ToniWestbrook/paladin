0688b327b817f62f34025194eb48990c420b4e1c
Matt MacManes <matthew.macmanes@unh.edu>
2015-07-24 08:37:01 -0400

    download fq

--- sample_data/make_test.sh
+++ sample_data/make_test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+wget https://s3.amazonaws.com/paladin.aligner/test.fq
 wget https://s3.amazonaws.com/paladin.aligner/paladin_test.faa
 ../paladin index -r2 paladin_test.faa
 ../paladin align -t4 -u2 paladin_test.faa test.fq > paladin_uniprot_report.txt
