bbd77ef21ec69da8a8342031b557d437f82ac8ac
Matt MacManes <matthew.macmanes@unh.edu>
2015-07-29 10:45:24 -0400

    test if non-zero file size

--- sample_data/make_test.sh
+++ sample_data/make_test.sh
@@ -5,7 +5,7 @@ curl -O https://s3.amazonaws.com/paladin.aligner/paladin_test.faa
 ../paladin index -r2 paladin_test.faa
 ../paladin align -t4 -u2 paladin_test.faa test.fq > paladin_uniprot_report.txt
 
-if [ -f paladin_uniprot_report.txt ];
+if [ -s paladin_uniprot_report.txt ];
         then
             echo "\n\nPALADIN HAS BEEN SUCCESSFULLY INSTALLED\n\n"
         else
