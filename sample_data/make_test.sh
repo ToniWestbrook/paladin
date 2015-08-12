00043f62b391ad5e2493182ab499f576d4f14112
Toni Westbrook <anthonyw@wildcats.unh.edu>
2015-08-12 00:21:32 -0400

    Update make_test.sh
    Updated make_test with new arguments

--- sample_data/make_test.sh
+++ sample_data/make_test.sh
@@ -3,9 +3,9 @@
 curl -O https://s3.amazonaws.com/paladin.aligner/test.fq
 curl -O https://s3.amazonaws.com/paladin.aligner/paladin_test.faa
 ../paladin index -r2 paladin_test.faa
-../paladin align -t4 -u2 paladin_test.faa test.fq > paladin_uniprot_report.txt
+../paladin align -t4 paladin_test.faa test.fq -o test
 
-if [ -s paladin_uniprot_report.txt ];
+if [ -s test_uniprot.tsv ];
         then
             echo "\n\nPALADIN HAS BEEN SUCCESSFULLY INSTALLED\n\n"
         else
