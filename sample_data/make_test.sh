3a44a6cfa849a88aaecb6f4a054b815fff2cf7be
Toni Westbrook <anthonyw@wildcats.unh.edu>
2015-12-21 23:40:00 -0500

    Update make_test.sh

--- sample_data/make_test.sh
+++ sample_data/make_test.sh
@@ -2,7 +2,8 @@
 
 curl -O https://s3.amazonaws.com/paladin.aligner/test.fq
 curl -O https://s3.amazonaws.com/paladin.aligner/paladin_test.faa
-../paladin index -r2 paladin_test.faa
+../paladin index -r3 paladin_test.faa
+../paladin prepare -r1 -f paladin_test.faa
 ../paladin align -t4 paladin_test.faa test.fq -o test
 
 if [ -s test_uniprot.tsv ];
